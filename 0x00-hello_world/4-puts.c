f293c09f4ce908fbfd689f345d456b42a59db5aa
ettaiykes <chidinmaiykes@gmail.com>
2022-10-15 16:57:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*
  * main - print a line using puts
  *
- * Return: O
+ * Return: 0
 */
 int main(void)
 {	
        puts("\"Programming is like building a multilingual puzzle");
-	return (O);
+	return (0);
 }	
