6f69f116c130a912f59a9507f566f2eb4db7988f
ettaiykes <chidinmaiykes@gmail.com>
2022-10-18 10:59:45 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
-
+#include “main.h”
 /**
- * main - prints _putchar and a new line
- * is written by etta
- * Reture: Always 0
+ *main -  print _putchar
+ *
+ *Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r);
-	_putchar('\n');
-	Reture (0);
+
+_putchar(‘_’);
+_putchar(‘p’);
+_putchar(‘u’);
+_putchar(‘t’);
+_putchar(‘c’);
+_putchar(‘h’);
+_putchar(‘a’);
+_putchar(‘r’);
+_putchar(‘\n’);
+return (0);
 }
