bbe3bc0b1564d469441f2e48371e67e12c452bb8
ettaiykes <chidinmaiykes@gmail.com>
2022-10-18 15:47:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar -writes the character c to stdout
+ * Qc: rhe character to print
+ *
+ * Return: on success 1
+ */
+int _putchar(char c)
+
+	return(write c1, & c,1));
+
